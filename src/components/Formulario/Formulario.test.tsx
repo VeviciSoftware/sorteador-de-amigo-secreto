ae1e65725566b70119c10fdaaff16a3263e3738b
Felipe Alexandre Duarte Araújo Ramos <felipeadar108@gmail.com>
2024-10-10 15:22:08 -0300

    :test_tube_ test: adicionando teste de adicionar usuário na lista caso exista um nome preenchido

--- src/components/Formulario/Formulario.test.tsx
+++ src/components/Formulario/Formulario.test.tsx
@@ -1,18 +1,47 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import React from "react";
 import Formulario from "./Formulario";
+import { RecoilRoot } from "recoil";
 
 //Jest
 
 test("quando o input esta vazio, novos participantes nao podem ser adicionados", () => {
-    //Arrange
-    render(<Formulario />);
+  //Arrange
+  render(
+    <RecoilRoot>
+      <Formulario />
+    </RecoilRoot>
+  );
 
-    //Act
-    const input = screen.getByPlaceholderText('Insira os nomes dos participantes');
-    const botao = screen.getByRole('button');
+  //Act
+  const input = screen.getByPlaceholderText(
+    "Insira os nomes dos participantes"
+  );
+  const botao = screen.getByRole("button");
 
-    //Assert
-    expect(input).toBeInTheDocument();
-    expect(botao).toBeDisabled();
+  //Assert
+  expect(input).toBeInTheDocument();
+  expect(botao).toBeDisabled();
+});
+
+test("adicionar um participante caso exista um nome preenchido", () => {
+  //Arrange
+  render(
+    <RecoilRoot>
+      <Formulario />
+    </RecoilRoot>
+  );
+
+  //Act
+  const input = screen.getByPlaceholderText(
+    "Insira os nomes dos participantes"
+  );
+  const botao = screen.getByRole("button");
+  fireEvent.change(input, { target: { value: "Lucas" } });
+  fireEvent.click(botao);
+
+  //Assert
+  expect(input).toHaveFocus();
+  expect(input).toHaveValue("");
+  expect(botao).toBeDisabled();
 });
--- src/components/Formulario/Formulario.tsx
+++ src/components/Formulario/Formulario.tsx
@@ -1,10 +1,34 @@
+import { useState, useRef } from "react";
+import { useAdicionarParticipante } from "../../state/hooks/useAdicionarParticipante";
+
 const Formulario = () => {
-    return (
-        <form>
-            <input type="text" placeholder="Insira os nomes dos participantes" />
-            <button type="submit" disabled>Adicionar</button>
-        </form>
-    );
+    
+    const [nome, setNome] = useState('')
+
+    const inputRef = useRef<HTMLInputElement>(null)
+
+    const adicionarNaLista = useAdicionarParticipante()
+
+    //const mensagemDeErro = useMensagemDeErro()
+    
+    const adicionarParticipante = (evento: React.FormEvent<HTMLFormElement>) => {
+        evento.preventDefault()
+        adicionarNaLista(nome)
+        setNome('')
+        inputRef.current?.focus()
+    }
+    
+    return (<form onSubmit={adicionarParticipante}>
+        <input
+            ref={inputRef}
+            value={nome}
+            onChange={evento => setNome(evento.target.value)}
+            type="text"
+            placeholder="Insira os nomes dos participantes"
+        />
+        <button disabled={!nome}>Adicionar</button>
+        {/* {mensagemDeErro && <p role="alert">{mensagemDeErro}</p>} */}
+    </form>)
 }
 
-export default Formulario;
+export default Formulario
